b430628e8713e61093116fbff7867edaa6e14b15
Bibek Regmi <vivekregmi83@gmail.com>
2025-11-01 22:13:25 +0545

    basic array questions added

--- src/dsa/array/basic/SingleNumber.java
+++ src/dsa/array/basic/SingleNumber.java
@@ -1,5 +1,26 @@
 package dsa.array.basic;
 
-public class SingleNumber {
+import java.util.HashMap;
+import java.util.Map;
 
+public class SingleNumber {
+    public static void main(String[] args) {
+
+    }
+
+    public int singleNumber(int[] arr){
+        Map<Integer, Integer> hashMap = new HashMap<>();
+
+        for(int a: arr){
+            hashMap.put(a, hashMap.getOrDefault(a, 0) +1);
+        }
+
+        // here return the only number that do not appear two times
+        for(Map.Entry<Integer, Integer> entry: hashMap.entrySet()){
+            if(entry.getValue() == 1){
+                return entry.getValue();
+            }
+        }
+        return -1;
+    }
 }
